ef6acb23a8a3a1e422cfaa0d1d52137a61e167da
unknown <nrv.rox.u@gmail.com>
2015-08-01 17:29:06 +0530

    NetLink Service added with broadcast reciever

--- app/src/main/java/nrv/com/ncastronomy/astromind/HomeActivity.java
+++ app/src/main/java/nrv/com/ncastronomy/astromind/HomeActivity.java
@@ -1,14 +1,19 @@
 package nrv.com.ncastronomy.astromind;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +26,25 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 
 /**
  * Created by NRV on 7/31/2015.
@@ -31,16 +55,18 @@ public class HomeActivity extends ActionBarActivity {
     private String[] dataset;
     private ActionBarDrawerToggle drawerToggle;
     private CustAdapter custAdapter;
+    private  ListView postlists;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startService(new Intent(this, NetLink.class));
         setContentView(R.layout.homepage);
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
 //        dataset=getResources().getStringArray(R.array.testar);
         custAdapter=new CustAdapter(this);
         listView=(ListView)findViewById(R.id.drawList);
         listView.setAdapter(custAdapter);
-
+        postlists=(ListView)findViewById(R.id.postlistviews);
 //        listView.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,dataset));
 
         drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close){
@@ -67,6 +93,44 @@ public class HomeActivity extends ActionBarActivity {
                 Toast.makeText(getApplication(),position+" Selected ",Toast.LENGTH_LONG).show();
                 listView.setItemChecked(position, true);
                 drawerLayout.closeDrawers();
+                Bundle bun=new Bundle();
+                bun.putString("fields","likes,message,picture");
+                if(position==1){//fetch
+                    new GraphRequest(
+                            AccessToken.getCurrentAccessToken(),
+                            "/1000407163303601/feed",
+                            bun,
+                            HttpMethod.GET,
+                            new GraphRequest.Callback() {
+                                public void onCompleted(GraphResponse response) {
+            /* handle the result */
+
+                                    try {
+                                        response.getRawResponse();
+                                        postlists.setAdapter(new PostViewAdapter(HomeActivity.this, response.getJSONObject()));
+                                        //Toast.makeText(getApplicationContext(),response.getRawResponse(),Toast.LENGTH_LONG).show();
+
+                                    }catch (Exception e){
+                                        postlists.setAdapter(new PostViewAdapter(HomeActivity.this,response.getJSONObject()));
+                                    }
+                                }
+                            }
+                    ).executeAsync();
+
+                    postlists.setVisibility(View.VISIBLE);
+
+                }
+                else if(position==2){
+                    postlists.setVisibility(View.INVISIBLE);
+                    // listView.setAdapter(null);
+
+                }
+                else if(position==3){
+                    Intent in=new Intent("Req.Store.Intent");
+                    //in.putExtra("Store_QTY",100.0);
+                    in.putExtra("Get_Store_Item","11111");
+                    sendBroadcast(in);
+                }
 
             }
         });
@@ -94,11 +158,13 @@ public class HomeActivity extends ActionBarActivity {
         drawerToggle.syncState();
     }
 
+
     }
 
 class CustAdapter extends  BaseAdapter{
 String menuItems[];
     int images[]={R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon};
+
     Context context;
     public CustAdapter(Context context_inp){
         context=context_inp;
@@ -152,6 +218,101 @@ String menuItems[];
             return row;
         }
     }
+}
+class SingleRow{
+    String title;
+    int likes;
+    String description;
+    int imageid;
+    int postimgid;
+
+    public SingleRow(String title, String description,int img,int iconimg) {
+        this.title = title;
+        this.description = description;
+        this.imageid=img;
+        this.postimgid=iconimg;
+    }
+}
+
+class PostViewAdapter extends BaseAdapter{
+    String[] titles;
+Context cont;
+    ArrayList<SingleRow> datalist;
+PostViewAdapter(Context con,JSONObject jsonObject){
+    cont=con;
+    datalist=new ArrayList<SingleRow>();
+    Resources res=con.getResources();
+    int images[]={R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon};
+    int images_icon[]={R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.posticon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon,R.drawable.icon};
+    if(jsonObject!=null){
+        try {
+            JSONArray data=jsonObject.getJSONArray("data");
+            for (int i=0;i<data.length();i++){
+                JSONObject post=data.getJSONObject(i);
+                String tit;
+                String desc;
+                if(post.has("message")){
+                    tit="message";
+                    desc=post.getString("message");
+                }
+                else{
+                    tit="story";
+                    desc=post.getString("story");
+                }
+                SingleRow s=new SingleRow(tit,desc,images[i],images_icon[i]);
+                datalist.add(s);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }/*
+    String[] titles=con.getResources().getStringArray(R.array.test2);
+    String[] desc=res.getStringArray(R.array.test2);
+
+    for (int i=0;i<5;i++){
+        SingleRow s=new SingleRow(titles[i],desc[i],images[i],images_icon[i]);
+        datalist.add(s);
+    }*/
+
+}
+    String[] title;
+    String[] desc;
+    int[] imgs;
+    @Override
+    public int getCount() {
+        return datalist.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return datalist.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater= (LayoutInflater) cont.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row=inflater.inflate(R.layout.post_row, parent, false);
+        ImageView imgicon= (ImageView) row.findViewById(R.id.posticon_image);
+        ImageView imgphoto= (ImageView) row.findViewById(R.id.post_image);
+        TextView titletext= (TextView) row.findViewById(R.id.post_desc_textview);
+        TextView nolikes= (TextView) row.findViewById(R.id.post_no_likes);
+        TextView post_pubdate= (TextView) row.findViewById(R.id.post_pubdate);
+        SingleRow temp=datalist.get(position);
+        imgicon.setImageResource(temp.postimgid);
+        imgphoto.setImageResource(temp.imageid);
+        titletext.setText(temp.description);
+        nolikes.setText(temp.title);
+        post_pubdate.setText(temp.title);
+        return row;
+    }
+
+
 }
 
 
---
+++ app/src/main/java/nrv/com/ncastronomy/astromind/NetLink.java
@@ -0,0 +1,70 @@
+package nrv.com.ncastronomy.astromind;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+/**
+ * Created by NRV on 8/1/2015.
+ */
+public class NetLink extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        IntentFilter movementFilter;
+        movementFilter = new IntentFilter("Req.Store.Intent");
+        getPostData getpostdata = new getPostData();
+        registerReceiver(getpostdata, movementFilter);
+        Log.d("hi","HiNet");
+
+        return START_STICKY;
+    }
+
+
+    class getPostData extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("hi","HiNetBroadcast");
+            Bundle bun=new Bundle();
+            bun.putString("fields","likes,message,picture");
+            new GraphRequest(
+                    AccessToken.getCurrentAccessToken(),
+                    "/1000407163303601/feed",
+                    bun,
+                    HttpMethod.GET,
+                    new GraphRequest.Callback() {
+                        public void onCompleted(GraphResponse response) {
+            /* handle the result */
+
+                            try {
+                                response.getRawResponse();
+                                Log.d("service",response.getRawResponse());
+                                //postlists.setAdapter(new PostViewAdapter(HomeActivity.this, response.getJSONObject()));
+                                //Toast.makeText(getApplicationContext(),response.getRawResponse(),Toast.LENGTH_LONG).show();
+
+                            }catch (Exception e){
+                                //postlists.setAdapter(new PostViewAdapter(HomeActivity.this,response.getJSONObject()));
+                            }
+                        }
+                    }
+            ).executeAsync();
+
+
+        }
+    }
+}
